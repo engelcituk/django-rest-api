816754035a3ef0c962456f2c2eaf0c4700961284
cituk <ecituk@thefives.com.mx>
2022-09-29 23:38:25 -0500

    :)

---
+++ watchmate/watchlist_app/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+class MovieSerializers(serializers.Serializer):
+    id = serializers.
+    name = serializers.
+    description = serializers.
+    active = serializers.
